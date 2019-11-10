88ec60558b025d38f7207c1b4d8fc66aec21cfd0
Enyao Gu <enyao.gu@ni.com>
2019-11-10 16:30:13 +0800

    Pan and spring

--- src/controls/CardWheel.jsx
+++ src/controls/CardWheel.jsx
@@ -1,51 +1,137 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
+import Hammer from 'react-hammerjs';
+import Bezier from 'bezier-js';
 import styles from './CardWheel.css';
 import Card from './Card.jsx';
-import { classNames } from '../utilities';
+
+/**
+ * Calculate the (x, y) of a position on the wheel circuit
+ * @param {Number} p_position the circuit position within range [-1, 1]
+ */
+function positionToXY(p_position) {
+	const coerced = Math.max(-1, Math.min(1, p_position));
+	const x = Math.sin((Math.PI / 2) * coerced);
+	const y = Math.cos((Math.PI / 2) * coerced);
+	return { x, y };
+}
+
+const springInertia = 1;
+const springTimeInMs = 300;
+function getSpringFunc(p_delta, p_velocity) {
+	const bezier = new Bezier([
+		{ x: 0, y: 0 },
+		{ x: 1, y: p_velocity },
+		{ x: 2, y: p_delta },
+		{ x: 3, y: p_delta },
+	]);
+	return (p_process) => bezier.get(p_process).y;
+}
+
+function useSpring(position, setPosition) {
+	const [springFunc, setSpringFunc] = useState({});
+	const [process, setProcess] = useState(0);
+	const springing = !!springFunc.calculatePosition;
+
+	function doSpring() {
+		const currentTime = Date.now();
+		requestAnimationFrame(() => {
+			const newProcess = process + (Date.now() - currentTime) / springTimeInMs;
+			setProcess(newProcess);
+		});
+	}
+
+	function startSpring(p_delta, p_velocity) {
+		setSpringFunc({ calculatePosition: getSpringFunc(p_delta, p_velocity)});
+	}
+
+	function stopSpring() {
+		if (!springing) { return; }
+		// Apply current spring distance into position
+		setPosition(position + springFunc.calculatePosition(Math.min(1, process)));
+		setSpringFunc({});
+		setProcess(0);
+	}
+
+	let springDistance = 0;
+	if (springing) {
+		if (process < 1) {
+			springDistance = springFunc.calculatePosition(process);
+			doSpring();
+		} else {
+			stopSpring();
+		}
+	}
+	return { springDistance, startSpring, stopSpring, springing };
+}
 
 export default function CardWheel({
-	height = 320,
-	width = 240,
+	height = 360,
+	width = 500,
+	vanishingAt = 160,
+	visiibleSide = 3,
+	cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 }) {
-	const [index, setIndex] = useState(0);
 	const wrapperStyle = {
 		'--height': `${height}px`,
 		'--width': `${width}px`,
 	};
 
-	const cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((card, key) => {
-		const [cardCovered, setCardCoverd] = useState(false);
-		const classes = [styles.item];
-		const diff = key - index;
-		switch (diff) {
-		case -2:
-			classes.push(styles.l2);
-			break;
-		case -1:
-			classes.push(styles.l1);
-			break;
-		case 0:
-			break;
-		case 1:
-			classes.push(styles.r1);
-			break;
-		case 2:
-			classes.push(styles.r2);
-			break;
-		default:
-			classes.push(diff > 0 ? styles.rx : styles.lx);
-			break;
-		}
+	const [position, setPosition] = useState(0);
 
-		return <div key={key} className={classNames(...classes)}>
-			<Card covered={cardCovered} onClick={() => {
-				if (!diff) {
-					setCardCoverd(!cardCovered);
-				} else {
-					setIndex(index + Math.sign(diff));
-				}
-			}}></Card>
+	// Handle panning and springFunc
+	const [panning, setPanning] = useState(false);
+	const [panDistance, setPanDistance] = useState(0);
+	const { springDistance, startSpring, stopSpring, springing } = useSpring(position, setPosition);
+	function handlePanStart() {
+		setPanning(true);
+		stopSpring();
+	}
+	function handlePan(p_event) {
+		setPanDistance(-p_event.deltaX * 2 / width);
+	}
+	function handlePanEnd(p_event) {
+		const endPanDistance = -p_event.deltaX * 2 / width;
+		const endPostion = position + endPanDistance;
+		setPanning(false);
+		// Merge panDistance in position
+		setPosition(endPostion);
+		setPanDistance(0);
+		// Start springFunc
+		const targetPosition = endPostion - p_event.velocityX * springInertia;
+		const springDelta = Math.round(Math.max(0, Math.min(cards.length, targetPosition))) - endPostion;
+		startSpring(springDelta, -p_event.velocityX);
+	}
+
+	const visualPosition = position + panDistance + springDistance;
+	const items = cards.map((card, key) => {
+		const itemPosition = key - visualPosition;
+		const { x: moveRatio, y: shrinkRatio } = positionToXY(itemPosition / (visiibleSide + 1));
+		const itemStyle = {
+			'--move-ratio': moveRatio * (1 - vanishingAt / width),
+			'--shrink-ratio': shrinkRatio,
+			zIndex: Math.ceil(10000 * shrinkRatio), // z-index must be an integer
+		};
+
+		const [cardCovered, setCardCoverd] = useState(false);
+		const focused = key === Math.round(visualPosition);
+
+		return <div key={key} className={styles.item} style={itemStyle}>
+			<div>
+				<Card covered={cardCovered} onClick={() => {
+					if (!panning && !springing && focused) {
+						setCardCoverd(!cardCovered);
+					}
+				}}></Card>
+			</div>
 		</div>;
 	});
-	return <div className={styles.wrapper} style={wrapperStyle}>{cards}</div>;
+	return <Hammer
+		direction='DIRECTION_HORIZONTAL'
+		onPanStart={handlePanStart}
+		onPan={handlePan}
+		onPanEnd={handlePanEnd}
+		onPanCancel={handlePanEnd}
+	><div className={styles.wrapper} style={wrapperStyle}>
+		{items}
+	</div></Hammer>
 }
